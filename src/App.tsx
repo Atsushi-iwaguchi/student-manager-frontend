3473386aca8f1a075fc42f05cf1a00fb4c5d7c17
Atsushi-iwaguchi <i.oneroom.bn@gmail.com>
2026-07-09 13:55:06 +0900

    Exam一覧作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "@/pages/LoginPage";
 import Dashboard from "@/pages/Dashboard"
 import RegisterPage from "./pages/RegisterPage";
+import ExamsPage from "./pages/ExamsPage";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/exams" element={<ExamsPage />} />
     </Routes>
   );
 }
--- src/pages/ExamsPage.tsx
+++ src/pages/ExamsPage.tsx
@@ -1,21 +1,37 @@
+import apiClient from "@/api/client";
 import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
+import { useAuth } from "@/contexts/AuthContext";
+import type { Exam } from "@/types";
 import { Plus } from "lucide-react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function ExamsPage() {
   const navigate = useNavigate();
+  const { user } = useAuth();
+  const [exams, setExams] = useState<Exam[]>([]);
+
+  useEffect(() => {
+    const fetchExams = async () => {
+      const response = await apiClient.get(`/students/${user?.id}/exams`);
+      setExams(response.data);
+    };
+    fetchExams();
+  }, [user]);
 
   return (
     <>
       <Header />
-      <div>
+      <div className="p-5">
         <ul>
-          <li>第1回全国模試</li>
+          {exams.map((exam) => (
+            <li key={exam.id}>{exam.exam_name}</li>
+          ))}
         </ul>
       </div>
       <div>
-        <Button>
+        <Button onClick={() => navigate("/exams/new") }>
           <Plus className="mr-2 h-4 w-4" />
           新規登録
         </Button>
