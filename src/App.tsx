d2c52c9e221ecb6363e14247637361b9232a5c3e
Atsushi-iwaguchi <i.oneroom.bn@gmail.com>
2026-07-09 11:40:45 +0900

    ユーザー登録機能作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "@/pages/LoginPage";
 import Dashboard from "@/pages/Dashboard"
+import RegisterPage from "./pages/RegisterPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
