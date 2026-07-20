eead13270b9e33b983b83f23ee028f305e031962
Atsushi-iwaguchi <i.oneroom.bn@gmail.com>
2026-07-20 10:52:47 +0900

    生徒画面のメニューを共通化

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,6 +6,12 @@ import { useNavigate } from "react-router-dom";
 export default function DashboardPage() {
   const { user } = useAuth();
   const navigate = useNavigate();
+  const MenuItems = [
+    { label: "模試成績一覧", path: "/exams" },
+    { label: "成績グラフ", path: "/graph" },
+    { label: "過去の面談", path: "/interview" },
+    { label: "面談予約", path: "/reservation" },
+  ];
 
   return (
     <>
@@ -14,34 +20,17 @@ export default function DashboardPage() {
       <div className="p-5">
         <p className="mb-5">{user?.name}さん今日も頑張りましょう！</p>
         <ul className="flex gap-4 flex-wrap justify-center">
-          <li>
-            <Button
-              variant="outline"
-              className="w-48 h-18"
-              onClick={() => navigate("/exams")}
-            >
-              模試成績一覧
-            </Button>
-          </li>
-          <li>
-            <Button
-              variant="outline"
-              className="w-48 h-18"
-              onClick={() => navigate("/graph")}
-            >
-              成績グラフ
-            </Button>
-          </li>
-          <li>
-            <Button variant="outline" className="w-48 h-18">
-              過去の面談
-            </Button>
-          </li>
-          <li>
-            <Button variant="outline" className="w-48 h-18">
-              面談予約
-            </Button>
-          </li>
+          {MenuItems.map((item) => (
+            <li key={item.path}>
+              <Button
+                variant="outline"
+                className="w-48 h-18"
+                onClick={() => navigate(item.path)}
+              >
+                {item.label}
+              </Button>
+            </li>
+          ))}
         </ul>
       </div>
     </>
